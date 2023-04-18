0874d71fc96a609383317f75876fc51f03ab76fd
tgartland89 <thomas.gartland@gmail.com>
2023-04-18 11:43:36 -0600

    dom element modification

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.getElementById('main').remove()
+const newHeader = document.createElement("h1");
+newHeader.id ='victory'
+newHeader.innerHTML = "Tom Gartland is the champion"
